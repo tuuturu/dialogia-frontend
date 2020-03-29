24207564759f5a7f2e4ba96b5d06baba30b1a0c6
Julius Pedersen <deifyed@gmail.com>
2020-03-29 19:47:04 +0200

    👌 Improve TagCloud

--- src/components/TagCloud.vue
+++ src/components/TagCloud.vue
@@ -37,11 +37,43 @@ export default {
 @import '~@/assets/theme.scss';
 
 .TagCloud {
+	max-width: 1200px;
+	margin: 3rem auto;
 	display: grid;
+	grid-template-columns: repeat(3, 1fr);
 
-	grid-template-columns: auto auto auto auto auto;
-	grid-row-gap: 4em;
+	@media screen and(min-width: 1000px) {
+		grid-template-columns: repeat(5, 1fr);
+	}
 
+	:nth-child(3n-2) {
+		grid-column: 1;
+	}
+	:nth-child(3n-1) {
+		grid-column: 3;
+	}
+	:nth-child(3n) {
+		grid-column: 2;
+	}
+	@media screen and (min-width: 1000px) {
+		:nth-child(5n-4) {
+			grid-column: 1;
+		}
+		:nth-child(5n-3) {
+			grid-column: 3;
+		}
+		:nth-child(5n-2) {
+			grid-column: 5;
+		}
+		:nth-child(5n-1) {
+			grid-column: 2;
+		}
+		:nth-child(5n) {
+			grid-column: 4;
+		}
+	}
+}
+.TagCloudWord {
 	color: $primary-color;
 }
 </style>
